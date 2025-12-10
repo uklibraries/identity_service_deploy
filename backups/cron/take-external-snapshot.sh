fc090ec5245e27ec22fb0d10f4d0f59de26549fc
MLE Slone <m.slone@gmail.com>
2025-12-10 10:31:57 -0500

    Adjust container state to check. Correct typo.

--- backups/cron/take-external-snapshot.sh
+++ backups/cron/take-external-snapshot.sh
@@ -5,6 +5,6 @@ set -e
 cd /home/deploy/services/identity
 source .env
 
-if [[ "$( docker container inspect -f '{{.State.Status}}' postgres_db )" == "true" ]]; then
+if [[ "$( docker container inspect -f '{{.State.Running}}' postgres-db )" == "true" ]]; then
     docker exec -t postgres_db /backups/take-snapshot.sh
 fi
